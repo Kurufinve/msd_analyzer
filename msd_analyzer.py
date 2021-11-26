ad839f41e13e095747392c0a08f1233d84bd2539
Kurufinve <poletaev.dan@gmail.com>
2021-11-27 00:44:30 +0300

    added functions get_unwrapped_structures and calc_non_averaged_msd (not tested) in small_fucntions.py

--- msd_analyzer.py
+++ msd_analyzer.py
@@ -11,7 +11,6 @@ rvats = os.listdir(path2rvat)
 rvat = rvats[0]
 st_parent = read_rv_at(path2rvat+rvat)
 
-# print(st_parent.i_mass_at)
 
 # reading the dumps of structures in r_at format (only coordinates of atoms)
 path2dumps = os.getcwd()+'/r_at/'
@@ -19,15 +18,20 @@ dumps = os.listdir(path2dumps)
 structures = []
 for dmp in dumps:
     st = read_r_at(path2dumps+dmp)
-    st.i_mass_at = st_parent.i_mass_at
-    st.i_at = st_parent.i_at
-    st.i_type_at = st_parent.i_type_at
-    st_numpy = convert_structure_to_numpy(st)
+    st.i_mass_at = st_parent.i_mass_at  # asigning atomic masses from parent rv_at structure
+    st.i_at = st_parent.i_at            # asigning atomic indexes from parent rv_at structure
+    st.i_type_at = st_parent.i_type_at  # asigning atomic types from parent rv_at structure
+    st.n_type_at = st_parent.n_type_at  # asigning atomic types from parent rv_at structure
+    st.type_at = st_parent.type_at  # asigning atomic types from parent rv_at structure
+    st_numpy = convert_structure_to_numpy(st) # converting attributes of structure class into the numpy array
     structures.append(st_numpy)
 
+# getting the list of structures with corrected center of mass 
 cm_corrected_structures = []
 for st in structures:
-    cm_corrected_st = get_cm_corrected_structure(st_numpy)
+    cm_corrected_st = get_cm_corrected_structure(st_numpy)  # correcting the position of center of mass in structure 
     cm_corrected_structures.append(cm_corrected_st)
 
-    
+# getting the list of structures with corrected center of mass and unwrapped positions
+cm_corrected_unwrapped_structures = get_unwrapped_structures(cm_corrected_structures) # unwrapping the atomic coordinates in structures
+
--- small_functions.py
+++ small_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import copy
 from md_format_converter_mi import structure
 
@@ -39,9 +40,9 @@ def get_cm_corrected_structure(st_numpy):
     cm_y = (st_numpy.i_mass_at*st_numpy.r_at[:,1]).sum()/st_numpy.i_mass_at.sum()
     cm_z = (st_numpy.i_mass_at*st_numpy.r_at[:,2]).sum()/st_numpy.i_mass_at.sum()
 
-    print(f"cm_x = {cm_x}")
-    print(f"cm_y = {cm_y}")
-    print(f"cm_z = {cm_z}")
+    # print(f"cm_x = {cm_x}")
+    # print(f"cm_y = {cm_y}")
+    # print(f"cm_z = {cm_z}")
 
     st_numpy_new = copy.deepcopy(st_numpy)
     st_numpy_new.r_at[:,0] = st_numpy.r_at[:,0] - cm_x
@@ -49,3 +50,147 @@ def get_cm_corrected_structure(st_numpy):
     st_numpy_new.r_at[:,2] = st_numpy.r_at[:,2] - cm_z
 
     return st_numpy_new
+
+def get_unwrapped_structures(sts_numpy):
+    """ 
+    Function for unwrapping the atomic coodinates 
+    for correct calculation of atomic displacements
+    get: sts_numpy - list of structure objects with attributes represented as numpy arrays
+    return: sts_numpy_new - list of structure objects with unwrapped coordinates
+    """
+
+    sts_numpy_new = copy.deepcopy(sts_numpy) # deep copy of list with structure objects
+    num_sts = len(sts_numpy) # number of structures
+    # sts_numpy_new[0] = copy.deepcopy(sts_numpy[0]) # copying the first structure in list
+    for i in range(1,num_sts):
+        # st_numpy_i = sts_numpy[i]
+        # st_numpy_i
+        shift = np.zeros((sts_numpy[i].n_at,3)) # initializing array of atom shift with zeros
+
+        sts_numpy_new[i] = copy.deepcopy(sts_numpy[i]) # copying the input structure to the output structure
+
+        r_at_wrapped = sts_numpy[i].r_at  # initial (wrapped within the periodic boundary conditions) coordinates of atoms
+
+        r_at_unwrapped = copy.deepcopy(r_at_wrapped)
+
+        dx_arr = sts_numpy[i].r_at[:,0] - sts_numpy[i-1].r_at[:,0] # array of diferrences of coordinates in x direction
+        dy_arr = sts_numpy[i].r_at[:,1] - sts_numpy[i-1].r_at[:,1] # array of diferrences of coordinates in y direction
+        dz_arr = sts_numpy[i].r_at[:,2] - sts_numpy[i-1].r_at[:,2] # array of diferrences of coordinates in z direction
+        for iat, dx, dy, dz in zip(range(len(sts_numpy[i].n_at)), dx_arr, dy_arr, dz_arr):                                                                                                                                                                                            5 vcvffrfv
+            if (dx >  sts_numpy[i-1].sizex/2): shift[iat,0] = shift[iat,0] - 0.5*(sts_numpy[i].sizex + sts_numpy[i-1].sizex)
+            if (dx < -sts_numpy[i-1].sizex/2): shift[iat,0] = shift[iat,0] + 0.5*(sts_numpy[i].sizex + sts_numpy[i-1].sizex)
+            if (dy >  sts_numpy[i-1].sizey/2): shift[iat,1] = shift[iat,1] - 0.5*(sts_numpy[i].sizey + sts_numpy[i-1].sizey)
+            if (dy < -sts_numpy[i-1].sizey/2): shift[iat,1] = shift[iat,1] + 0.5*(sts_numpy[i].sizey + sts_numpy[i-1].sizey)
+            if (dz >  sts_numpy[i-1].sizez/2): shift[iat,2] = shift[iat,2] - 0.5*(sts_numpy[i].sizez + sts_numpy[i-1].sizez)
+            if (dz < -sts_numpy[i-1].sizez/2): shift[iat,2] = shift[iat,2] + 0.5*(sts_numpy[i].sizez + sts_numpy[i-1].sizez) 
+
+        r_at_unwrapped[:,0] = sts_numpy_new[i-1].r_at[:,0] + (sts_numpy[i].r_at[:,0] + shift[:,0] - sts_numpy[i-1].r_at[:,0])     
+        r_at_unwrapped[:,1] = sts_numpy_new[i-1].r_at[:,1] + (sts_numpy[i].r_at[:,1] + shift[:,1] - sts_numpy[i-1].r_at[:,1])     
+        r_at_unwrapped[:,2] = sts_numpy_new[i-1].r_at[:,2] + (sts_numpy[i].r_at[:,2] + shift[:,2] - sts_numpy[i-1].r_at[:,2])     
+
+        sts_numpy_new[i].r_at = r_at_unwrapped
+
+    return sts_numpy_new
+
+
+def calc_non_averaged_msd(sts_numpy, dt):
+    """
+    This function calculates the mean-squared displacements (msd) of atoms in structures from sts_numpy list
+    with respect to the first structure in sts_numpy list.
+    get: sts_numpy, dt - list of structure objects with unwrapped coordinates and time difference in ps between structures, respectively
+    return: msd_arr - num_sts*(5+4*n_type_at) array, where the first column is time, 
+            the second column is non-averaged msd of all atoms,
+            the third column is non-averaged msd of all atoms in x direction,
+            the fourth column is non-averaged msd of all atoms in y direction,
+            the fifth column is non-averaged msd of all atoms in z direction,
+            the sixth and other columns are non-averaged msds of atoms of i_type i
+
+    return pandas dataframe        
+    """
+
+    msd_r = {}
+    msd_x = {} 
+    msd_y = {} 
+    msd_z = {} 
+
+    msd_r['all'] = []
+    msd_x['all'] = []
+    msd_y['all'] = []
+    msd_z['all'] = []
+
+    for i_type in sts_numpy[0].type_at:
+        msd_r[str(i_type)] = []
+        msd_x[str(i_type)] = []
+        msd_y[str(i_type)] = []
+        msd_z[str(i_type)] = []
+
+    num_sts = len(sts_numpy)
+    for i in range(num_sts):
+        msd_x_i = ((sts_numpy[i].r_at[:,0] - sts_numpy[0].r_at[:,0])**2).sum()/sts_numpy[i].n_at
+        msd_y_i = ((sts_numpy[i].r_at[:,1] - sts_numpy[0].r_at[:,1])**2).sum()/sts_numpy[i].n_at
+        msd_z_i = ((sts_numpy[i].r_at[:,2] - sts_numpy[0].r_at[:,2])**2).sum()/sts_numpy[i].n_at
+        msd_r_i = msd_x_i + msd_y_i + msd_z_i
+
+        msd_x['all'].append(msd_x_i)
+        msd_y['all'].append(msd_y_i)
+        msd_z['all'].append(msd_z_i)
+        msd_r['all'].append(msd_r_i)
+
+        for i_type in sts_numpy[0].type_at:
+            mask = sts_numpy[i].i_type_at == i_type
+
+            msd_x_i = ((sts_numpy[i].r_at[:,0][mask] - sts_numpy[0].r_at[:,0][mask])**2).sum()/mask.sum()
+            msd_y_i = ((sts_numpy[i].r_at[:,1][mask] - sts_numpy[0].r_at[:,1][mask])**2).sum()/mask.sum()
+            msd_z_i = ((sts_numpy[i].r_at[:,2][mask] - sts_numpy[0].r_at[:,2][mask])**2).sum()/mask.sum()
+            msd_r_i = msd_x_i + msd_y_i + msd_z_i
+
+            msd_x[str(i_type)].append(msd_x_i)
+            msd_y[str(i_type)].append(msd_y_i)
+            msd_z[str(i_type)].append(msd_z_i)
+            msd_r[str(i_type)].append(msd_r_i)           
+
+    msd = {'msd_r': msd_r, 'msd_x': msd_x, 'msd_y': msd_y, 'msd_z': msd_z}
+
+    msd_df = pd.DataFrame(msd)
+
+    return msd_df
+
+
+    n_time_diff=n_write-1
+
+	do i_time_diff=1,n_time_diff
+
+	ww=0.0D0
+	ww_sort=0.0D0
+	do i_start=1,n_write-i_time_diff
+	i_end=i_start+i_time_diff
+
+	    w=0.0D0
+	    w_sort=0.0D0
+	    n_at_sort=0
+	    do i_at=1,n_at
+	    d =     (r_at_set(1,i_at,i_end)-r_at_set(1,i_at,i_start))**2 + 
+     :		(r_at_set(2,i_at,i_end)-r_at_set(2,i_at,i_start))**2 + 
+     :		(r_at_set(3,i_at,i_end)-r_at_set(3,i_at,i_start))**2
+	    w=w+d
+	    i_sort=i_sort_at(i_at)
+	    w_sort(i_sort)=w_sort(i_sort)+d
+	    n_at_sort(i_sort)=n_at_sort(i_sort)+1
+    	    enddo
+    	w=w/dfloat(n_at)
+	ww=ww+w
+	    do i_sort=1,n_sort
+	    if(n_at_sort(i_sort).gt.0) then
+    	    w_sort(i_sort)=w_sort(i_sort)/dfloat(n_at_sort(i_sort))
+	    ww_sort(i_sort)=ww_sort(i_sort)+w_sort(i_sort)
+	    endif
+	    enddo
+	enddo
+	ww=ww/dfloat(n_write-i_time_diff)
+    	dr_at(i_time_diff)=ww
+	    do i_sort=1,n_sort
+	    if(n_at_sort(i_sort).gt.0) then
+	    dr_at_sort(i_time_diff,i_sort)=ww_sort(i_sort)/dfloat(n_write-i_time_diff)
+	    endif
+	    enddo
+	enddo
